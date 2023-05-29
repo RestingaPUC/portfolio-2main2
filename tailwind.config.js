cd1c77c31fd2ac579980a14eb85b28ab78330355
Alberto <carneiroalberto@yahoo.com.br>
2023-05-29 20:56:44 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,mdx}",],
+  theme: {
+    extend: {
+      colors: {
+        headerColor: "hsl(271, 58%, 18%)",
+      },
+    },
+  },
+  plugins: [],
+}
+
